6623359de2c220a3912044fb8c91d3262cfde6bf
tester <dmfrodol@yandex.ru>
2019-11-19 03:30:19 -0800

    move validation code to separate methods

--- carriage.rb
+++ carriage.rb
@@ -20,8 +20,16 @@ class Carriage
   protected
 
   def validate!
+    validate_carriage_name_type!
+    validate_carriage_name_length!
+  end
+
+  def validate_carriage_name_type!
     type_message = "Wrong carriage name! Should be string, got #{@number.class}"
     raise RailwayError, type_message unless @number.is_a?(String)
+  end
+
+  def validate_carriage_name_length!
     length_message = "Carriage number should be between 3 and 20 symbols! Got - #{@number.length}"
     raise RailwayError, length_message unless 3 < @number.length && @number.length < 20
   end
